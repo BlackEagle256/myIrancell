58b2bc43a497d44f619d53ece3278128b89aaec8
mohammadHosein <mohammadhoseindadgostr@gmail.com>
2025-06-12 01:26:01 +0330

    Added Users Api To Index.html File

---
+++ frontend/content/js/userInfo.js
@@ -0,0 +1,26 @@
+const $ = document;
+
+window.addEventListener('load', () => {
+    const userProfile = $.querySelector('.user-profile');
+    const userPersonalInfo = $.querySelector('.user-name');
+    const userNumber = $.querySelector('.user-number');
+    const userCharge = $.querySelector('.overlay');
+
+    const mainUrlApi = 'http://localhost:3000/api';
+
+    let userToken = localStorage.getItem('user-token');
+
+    fetch(`${mainUrlApi}/users`, {
+        headers: {
+            authorization: userToken
+        }
+    })
+        .then(res => res.json())
+        .then(user => {
+            console.log(user)
+            userProfile.setAttribute('src', user[0].profile);
+            userPersonalInfo.innerHTML = `سلام ${user[0].firstname} ${user[0].lastname}`;
+            userNumber.innerHTML = user[0].phone;
+            userCharge.innerHTML = `${user[0].charge} تومان`;
+        })
+})
